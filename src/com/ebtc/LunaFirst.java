3e80e1453a0147d98fd9bfc02dc054115f467121
Administrator <Administrator@Asanka-PC>
2014-10-29 22:31:09 +0530

    patch commit

--- src/com/ebtc/LunaFirst.java
+++ src/com/ebtc/LunaFirst.java
@@ -4,6 +4,6 @@ public class LunaFirst {
 
 	 public static void main(String[] args) {
 		System.out.println("Welcome to Luna");
-		System.out.println("Edited Here Go");
+		System.out.println("Edited Here Go Patch");
 	}
 }
