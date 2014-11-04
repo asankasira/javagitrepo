16cdd403ea9b16c49d9219c4b22ad2eefc8a4dd7
Asanka Siriwardena <asankabg@gmail.com>
2014-11-04 23:34:38 +0530

    push to origin

--- src/com/ebtc/LunaFirst.java
+++ src/com/ebtc/LunaFirst.java
@@ -16,12 +16,10 @@ public class LunaFirst {
 	public void sharedMethod() {
 		System.out.println(" GitHub made this. ");
 		System.out.println(" Luna made change ");
-		System.out.println(" GitHub made this. ");
 	}
 
 	public void testM() {
 		System.out.println("GitHub test");
 		System.out.println("Luna test");
-		System.out.println("GitHub test");
 	}
 }
