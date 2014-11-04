b8d12e5860a3c701a2c05623702d5e6aa64dec2a
Asanka Siriwardena <asankabg@gmail.com>
2014-11-04 23:28:16 +0530

    Luna 2nd rebase test.

--- src/com/ebtc/LunaFirst.java
+++ src/com/ebtc/LunaFirst.java
@@ -2,24 +2,22 @@ package com.ebtc;
 
 public class LunaFirst {
 
-	 public static void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Welcome to Luna");
 
 		System.out.println("Local Master Go");
 
 		System.out.println("Master Remote Edit Here");
-		
+
 		int k = 0;
 
 	}
 
-	public void sharedMethod(){
-	    System.out.println(" GitHub made this. ");
+	public void sharedMethod() {
 		System.out.println(" Luna made change ");
-        }
+	}
 
-	public void testM(){
-		System.out.println("GitHub test");
+	public void testM() {
 		System.out.println("Luna test");
 	}
 }
